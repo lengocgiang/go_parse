e2865b35254e1dbfcff1a1330f6762de00992800
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2017-05-19 16:31:42 +0700

    init

---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	URL := "localhost:8080/bk/api/actions.json?distinct_id=22397_1495093020959_3130&timestamp=1495093225&event=update_cart_source&params%5Bcart_token%5D=4edf5bb6c3bd13844a95614e7cfecb10&params%5Bsources%5D%5B%5D=mobilewebboost_conversion_driver&params%5Bsources%5D%5B%5D=mobilewebboost_app&params%5Bsources%5D%5B%5D=mobilewebboost_web&params%5BclientDevice%5D=mobile&params%5BclientIsMobile%5D=true&params%5BclientIsSmallScreen%5D=true&api_key=8eb697157e35c4b20e158328de64aac7"
+
+}
